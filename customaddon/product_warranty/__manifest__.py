942b390f23600c0a3b3ef3119ae5abd1a3aa80ce
BuiDatBk <buivandaty2k@gmail.com>
2022-12-19 16:57:03 +0700

    custom_font_end

--- customaddon/product_warranty/__manifest__.py
+++ customaddon/product_warranty/__manifest__.py
@@ -28,6 +28,7 @@
         'views/product_template_inherit.xml',
         'security/groups.xml',
         'views/sale_order_line_inherit.xml',
+        'views/website_sale_delivery_inherit.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
